0f9464a218b8b0abbaaf1c62a81270e1778f60b4
sarveshrane2000 <sarveshrane2000@gmail.com>
2022-08-01 04:18:36 +0530

    Update: settings

--- src/Website/Website/settings.py
+++ src/Website/Website/settings.py
@@ -145,3 +145,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+import django_heroku
+django_heroku.settings(locals())
